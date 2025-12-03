34ebaca3f3f48d22e6e1b2f43f376a0b8ee0daf6
Reyli2519 <reyliandersongonzalezdiaz@gmail.com>
2025-12-02 20:21:38 -0400

    Add registration form to register.php
    agrego datos que faltaban

--- views/register.php
+++ views/register.php
@@ -4,4 +4,15 @@
     <title>Registro - Biblioteca</title>
 </head>
 <body>
-    <h
+    <h1>Registro</h1>
+    <form method="POST" action="index.php?action=register">
+        Email: <input type="email" name="email" required><br>
+        Contraseña: <input type="password" name="password" required minlength="6"><br>
+        Nombre: <input type="text" name="nombre" required><br>
+        Apellido: <input type="text" name="apellido" required><br>
+        <button type="submit">Registrar</button>
+    </form>
+    <p><?php echo $error; ?></p>
+    <a href="index.php?action=login">Iniciar Sesión</a>
+</body>
+</html>
